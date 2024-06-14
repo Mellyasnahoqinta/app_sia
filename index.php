90b417c8021816e0b68494716ef6c347e2183644
Mellyasnahoqinta <melivivo297@gmail.com>
2024-06-14 17:54:16 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,132 +1,56 @@
-
-<div class="card">
-    <div class="card-body">
-        <div class="row">
-            <div class="mb-3 col-md-6">
-                <label for="username" class="form-label">username</label>
-                <input type="text" class="from-control" name="username">
-</div>
-<div class="mb-3 col-md-6">
-    <label for="password" class="from-label">password</label>
-    <input type="text" class="from control" name="password">
-</div>
-</row>
-<div class="row">
-    <div class="mb-3 col-md-6">
-        <label for="level" class="from-lebel">level</label>
-        <input type="text" class="from control" name="level">
-</div>
-<div class="mb-3 col-md-6">
-<label for="email" class="from-lebel">email</label>
-<input type="text" class="from control" name="email">
-</div>
-</div>
-<hr>
-<div class="row">
-    <div class="col text-end">
-    <button class="btn btn-secondary" type="reset">Reset</button>
- <button class="btn btn-primary" type="submit">Simpan</button>
+<?php
+ $query = "SELECT * FROM tbl_pengguna WHERE username='$_SESSION[username]'";
+ $exec = mysqli_query($koneksi, $query);
+ $data = mysqli_fetch_array($exec);
+ ?>
+ <div class="card mb-3">
+     <div class="card-body">
+ <form action="modul/profile/aksi_profile.php" method="post">
+ <div class="row">
+ <div class="mb-3 col-md-4">
+ <label for="username" class="form-label">Username</label>
+ <input type="text" class="form-control" name="username" value="<?=
+$data['username'] ?>" readonly>
  </div>
+ <div class="mb-3 col-md-4">
+ <label for="nama_lengkap" class="form-label">Nama lengkap</label>
+ <input type="text" class="form-control" name="nama_lengkap"
+value="<?= $data['nama_lengkap'] ?>">
  </div>
- <div class="card">
- <div class="card-header">
- <h3>Data Pengguna</h3>
- <div class="card-body">
- <div class="table-responsive">
- <table class="table table-striped">
- <thead>
- <tr>
- <th>#</th>
- <th>Username</th>
- <th>Level</th>
- <th>Email</th>
- <th><i class="bi bi-gear-fill"></i></th>
- </tr>
- </thead>
- <tbody>
- <tr>
- <td>1</td>
- <td>admin</td>
- <td>admin</td>
- <td>admin@gmail.com</td>
- <td>
- <a href="#editPengguna" class="text-decoration-none" data-
-bs-toggle="modal">
- <i class="bi bi-pencil-square text-success"></i>
- </a>
- <a href="" class="text-decoration-none">
- <i class="bi bi-trash text-danger"></i>
- </a>
- </td>
- <!-- Modal -->
- <div class="modal fade" id="editPengguna" tabindex="-1" aria-
-labelledby="editPenggunaLabel" aria-hidden="true">
- <div class="modal-dialog">
- <div class="modal-content">
- <div class="modal-header">
- <h5 class="modal-title" id="editPenggunaLabel">Edit
-Pengguna</h5>
- <button type="button" class="btn-close" data-bs-
-dismiss="modal" aria-label="Close"></button>
- </div>
- <div class="modal-body">
- <div class="mb-3">
- <label for="username" class="form-
-label">Username</label>
- <input type="text" class="form-control"
-name="username" value="admin">
- </div>
- <div class="mb-3">
- <label for="password" class="form-
-label">Password</label>
- <input type="text" class="form-control"
-name="password">
- <span class="form-text text-muted">Kosongkan jika
-tidak ingin mengganti password</span>
- </div>
- <div class="mb-3">
- <label for="level" class="form-label">Level</label>
- <select class="form-select" name="level">
- <option value="admin">Admin</option>
- <option value="user">User</option>
- </select>
- </div>
- <div class="mb-3">
+ <div class="mb-3 col-md-4">
  <label for="email" class="form-label">Email</label>
- <input type="text" class="form-control" name="email"
-value="admin@gmail.com">
+ <input type="email" class="form-control" name="email" value="<?=
+$data['email'] ?>">
  </div>
  </div>
- <div class="modal-footer">
- <button type="button" class="btn btn-secondary" data-
-bs-dismiss="modal">Close</button>
- <button type="button" class="btn btn-
-primary">Simpan</button>
+ <div class="row">
+ <div class="mb-3 col-md-4">
+ <label for="password" class="form-label">Password</label>
+ <input type="password" class="form-control" name="password">
+ </div>
+ <div class="mb-3 col-md-4">
+ <label for="password_baru" class="form-label">Password
+baru</label>
+ <input type="password" class="form-control" name="password_baru">
+ </div>
+ <div class="mb-3 col-md-4">
+ <label for="password_ulang" class="form-label">Ulangi
+password</label>
+ <input type="password" class="form-control" name="password_ulang">
  </div>
  </div>
+ <hr class="text-secondary">
+ <div class="d-flex">
+ <span class="me-auto text-gray">
+ <?php
+ if(isset($_SESSION['pesan'])){
+ echo $_SESSION['pesan'];
+ unset($_SESSION['pesan']);
+ }
+ ?>
+ </span>
+ <button type="submit" name="submit" class="btn btnprimary">Perbaharui</button>
  </div>
- </div>
- </tr>
- <tr>
- <td>2</td>
- <td>user</td>
- <td>user</td>
- <td>user@gmail.com</td>
- <td>
- <a href="#editPengguna" class="text-decoration-none"
-data-bs-toggle="modal">
- <i class="bi bi-pencil-square text-success"></i>
- </a>
-<a href="" class="text-decoration-none">
- <i class="bi bi-trash text-danger"></i>
- </a>
- </td>
- </tr>
- </tbody>
- </table>
- </div>
- </div>
- </div>
- </div>
+ </form>
  </div>
  </div>
