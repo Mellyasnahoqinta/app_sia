ce6b91e5e7649535003109583c7512bd77a604dd
Mellyasnahoqinta <melivivo297@gmail.com>
2024-06-14 20:05:54 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,155 +1,180 @@
-<form action="modul/akun/aksi_akun.php?act=insert" method="post">
 <div class="card mb-3">
-     <div class="card-body">
- <form action="" method="post">
- <div class="row mb-3">
- <div class="col-md-4">
- <label class="form-label" for="nama_akun">Nama akun</label>
- <input type="text" class="form-control" name="nama_akun">
- </div>
- <div class="col-md-4">
- <label class="form-label" for="jenis_akun">Jenis akun</label>
- <input type="text" class="form-control" name="jenis_akun">
- </div>
- <div class="col-md-4">
- <label class="form-label" for="type_saldo">Type saldo</label>
- <select class="form-select" name="type_saldo">
- <option value="debit">Debit</option>
- <option value="kredit">Kredit</option>
- </select>
- </div>
- </div>
- <hr class="text-secondary">
- <div class="text-end">
- <button type="reset" class="btn btn-secondary">Reset</button>
- <button type="submit" class="btn btn-primary">Simpan</button>
- </div>
- </form>
- </div>
+    <?php
+        // Check if the form has been submitted
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Get the form data
+            $nama_barang = $_POST["nama_barang"];
+            $harga_beli = $_POST["harga_beli"];
+            $harga_jual = $_POST["harga_jual"];
+            $stok = $_POST["stok"];
+
+            // Validate the form data
+            if (!empty($nama_barang) && !empty($harga_beli) && !empty($harga_jual) && !empty($stok)) {
+                // Save the data to a database or file
+              // ...
+
+                // Display a success message
+                echo "<div class='card-body'>";
+                echo "<p class='text-success'>Berhasil menambahkan data barang!</p>";
+                echo "</div>";
+            } else {
+                // Display an error message
+                echo "<div class='card-body'>";
+                echo "<p class='text-danger'>Harap isi semua kolom dengan benar!</p>";
+                echo "</div>";
+            }
+        }
+    ?>
+    <form action="" method="post">
+        <div class="card-body">
+            <div class="row">
+                <div class="mb-3 col-md-6">
+                    <label for="nama_barang" class="form-label"> Nama barang</label>
+                    <input type="text" class="form-control" name="nama_barang">
+                </div>
+                <div class="mb-3 col-md-6">
+                    <label for="harga_beli" class="form-label"> Harga beli</label>
+                    <input type="number" class="form-control" name="harga_beli">
+                </div>
+            </div>
+            <div class="row">
+                <div class="mb-3 col-md-6">
+                    <label for="harga_jual" class="form-label">Harga jual</label>
+                    <input type="number" class="form-control" name="harga_jual">
+                </div>
+                <div class="mb-3 col-md-6">
+                    <label for="stok" class="form-label"> Stok</label>
+                    <input type="number" class="form-control" name="stok">
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="text-end">
+            <button type="submit" class="btn btn-primary">Simpan</button>
+        </div>
+    </form>
 </div>
- <div class="card">
- <div class="card-header">
- <h3>Data Akun</h3>
- </div>
- <div class="card-body">
- <div class="table-responsive">
- <table class="table table-striped">
- <thead>
- <tr>
- <th>#</th>
- <th>Nama Akun</th>
- <th>Jenis Akun</th>
- <th>Type Saldo</th>
- <th><i class="bi bi-gear-fill"></i></th>
- </tr>
- </thead>
- <tbody>
- <tr>
- <td>1</td>
- <td>Kas</td>
- <td>Aset</td>
- <td>Debit</td>
- <td>
- <a href="#editAkun" class="text-decoration-none" data-bstoggle="modal">
- <i class="bi bi-pencil-square text-success"></i>
-</a>
- <a href="" class="text-decoration-none">
- <i class="bi bi-trash text-danger"></i>
- </a>
- </td>
- <!-- Modal -->
- <div class="modal fade" id="editAkun" tabindex="-1" arialabelledby="exampleModalLabel" aria-hidden="true">
- <form action="" method="post">
- <div class="modal-dialog">
- <div class="modal-content">
- <div class="modal-header">
-<h1 class="modal-title fs-5" id="exampleModalLabel">Edit Data Akun</h1>
- <button type="button" class="btn-close" data-bsdismiss="modal" aria-label="Close"></button>
- </div>
- <div class="mb-3">
- <label class="form-label" for="nama_akun">Namaakun</label>
- <input type="text" class="form-control" name="nama_akun"value="Kas">
- </div>
- <div class="mb-3">
- <label class="form-label" for="jenis_akun">Jenisakun</label>
- <input type="text" class="form-control"name="jenis_akun" value="Aset">
- </div>
- <div class="mb-3">
- <label class="form-label" for="type_saldo">Type saldo</label>
- <select class="form-select" name="type_saldo">
- <option value="debit" selected>Debit</option>
- <option value="kredit">Kredit</option>
- </select>
- </div>
- </div>
- <div class="modal-footer">
- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
- <button type="submit" class="btn btnprimary">Simpan</button>
- </div>
- </div>
- </div>
- </form>
- </div>
- </tr>
- <tr>
- <td>2</td>
- <td>Piutang Usaha</td>
- <td>Aset</td>
- <td>Debit</td>
- <td>
- <a href="" class="text-decoration-none">
- <i class="bi bi-pencil-square text-success"></i>
- </a>
- <a href="" class="text-decoration-none">
-     <i class="bi bi-trash text-danger"></i>
- </a>
- </td>
- </tr>
- <tr>
- <td>3</td>
- <td>Perlengkapan</td>
- <td>Aset</td>
- <td>Debit</td>
- <td>
- <a href="" class="text-decoration-none">
- <i class="bi bi-pencil-square text-success"></i>
- </a>
- <a href="" class="text-decoration-none">
- <i class="bi bi-trash text-danger"></i>
- </a>
- </td>
- </tr>
-<tr>
- <td>4</td>
- <td>Utang Usaha</td>
- <td>Kewajiban</td>
- <td>Kredit</td>
- <td>
- <a href="" class="text-decoration-none">
- <i class="bi bi-pencil-square text-success"></i>
- </a>
- <a href="" class="text-decoration-none">
- <i class="bi bi-trash text-danger"></i>
- </a>
- </td>
- </tr>
- <tr>
- <td>5</td>
- <td>Ekuitas</td>
- <td>Modal</td>
- <td>Kredit</td>
- <td>
- <a href="" class="text-decoration-none">
- <i class="bi bi-pencil-square text-success"></i>
- </a>
- <a href="" class="text-decoration-none">
- <i class="bi bi-trash text-danger"></i>
- </a>
- </td>
- </tr>
- </form>
- </tbody>
- </table>
- </div>
- </div>
- </div>
+<div class="card mb-3">
+  <div class="card-body">
+    <form action="simpan_barang.php" method="post">
+      <div class="row">
+        <div class="mb-3 col-md-6">
+          <label class="form-label" for="nama_barang">Barang</label>
+          <input type="text" class="form-control" name="nama_barang">
+        </div>
+        <div class="mb-3 col-md-6">
+          <label class="form-label" for="harga_beli">Harga beli</label>
+          <input type="number" class="form-control" name="harga_beli">
+        </div>
+      </div>
+      <div class="row">
+        <div class="mb-3 col-md-6">
+          <label class="form-label" for="harga_jual">Harga jual</label>
+          <input type="number" class="form-control" name="harga_jual">
+        </div>
+        <div class="mb-3 col-md-6">
+          <label class="form-label" for="stok">Stok</label>
+          <input type="number" class="form-control" name="stok">
+        </div>
+      </div>
+      <div class="col text-end">
+        <button class="btn btn-secondary" type="reset">Reset</button>
+        <button class="btn btn-primary" type="submit">Simpan</button>
+      </div>
+    </form>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-header">
+    <h3>Data Barang</h3>
+  </div>
+  <div class="card-body">
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Barang</th>
+            <th>Harga beli</th>
+            <th>Harga jual</th>
+            <th>Stok</th>
+            <th><i class="bi bi-gear-fill"></i></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Laptop ASUS</td>
+            <td>Rp. 10.000.000</td>
+            <td>Rp. 12.000.000</td>
+            <td>5</td>
+            <td>
+              <a href="#editBarang" class="text-decoration-none" data-bs-toggle="modal">
+                <i class="bi bi-pencil-square text-success"></i>
+              </a>
+              <a href="" class="text-decoration-none">
+                <i class="bi bi-trash text-danger"></i>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Monitor Samsung</td>
+            <td>Rp. 5.000.000</td>
+            <td>Rp. 6.000.000</td>
+            <td>10</td>
+            <td>
+              <a href="#editBarang" class="text-decoration-none" data-bs-toggle="modal">
+                <i class="bi bi-pencil-square text-success"></i>
+              </a>
+              <a href="" class="text-decoration-none">
+                <i class="bi bi-trash text-danger"></i>
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="editBarang" tabindex="-1" arialabelledby="exampleModalLabel" arial-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Edit Barang</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" arialabel="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="simpan_barang.php" method="post">
+          <div class="row">
+            <div class="mb-3 col-md-6">
+              <label class="form-label" for="nama_barang">Barang</label>
+              <input type="text" class="form-control" name="nama_barang">
+            </div>
+            <div class="mb-3 col-md-6">
+              <label class="form-label" for="harga_beli">Harga beli</label>
+              <input type="number" class="form-control" name="harga_beli">
+            </div>
+          </div>
+          <div class="row">
+            <div class="mb-3 col-md-6">
+              <label class="form-label" for="harga_jual">Harga jual</label>
+              <input type="number" class="form-control" name="harga_jual">
+            </div>
+            <div class="mb-3 col-md-6">
+              <label class="form-label" for="stok">Stok</label>
+              <input type="number" class="form-control" name="stok">
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btnprimary">Simpan</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
