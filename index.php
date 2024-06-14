26fba2f5595f60d4df4863bd6e30561139f1be3c
Mellyasnahoqinta <melivivo297@gmail.com>
2024-06-14 17:54:41 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,56 +1,82 @@
-<?php
- $query = "SELECT * FROM tbl_pengguna WHERE username='$_SESSION[username]'";
- $exec = mysqli_query($koneksi, $query);
- $data = mysqli_fetch_array($exec);
- ?>
- <div class="card mb-3">
-     <div class="card-body">
- <form action="modul/profile/aksi_profile.php" method="post">
- <div class="row">
- <div class="mb-3 col-md-4">
- <label for="username" class="form-label">Username</label>
- <input type="text" class="form-control" name="username" value="<?=
-$data['username'] ?>" readonly>
- </div>
- <div class="mb-3 col-md-4">
- <label for="nama_lengkap" class="form-label">Nama lengkap</label>
- <input type="text" class="form-control" name="nama_lengkap"
-value="<?= $data['nama_lengkap'] ?>">
- </div>
- <div class="mb-3 col-md-4">
- <label for="email" class="form-label">Email</label>
- <input type="email" class="form-control" name="email" value="<?=
-$data['email'] ?>">
- </div>
- </div>
- <div class="row">
- <div class="mb-3 col-md-4">
- <label for="password" class="form-label">Password</label>
- <input type="password" class="form-control" name="password">
- </div>
- <div class="mb-3 col-md-4">
- <label for="password_baru" class="form-label">Password
-baru</label>
- <input type="password" class="form-control" name="password_baru">
- </div>
- <div class="mb-3 col-md-4">
- <label for="password_ulang" class="form-label">Ulangi
-password</label>
- <input type="password" class="form-control" name="password_ulang">
- </div>
- </div>
- <hr class="text-secondary">
- <div class="d-flex">
- <span class="me-auto text-gray">
- <?php
- if(isset($_SESSION['pesan'])){
- echo $_SESSION['pesan'];
- unset($_SESSION['pesan']);
- }
- ?>
- </span>
- <button type="submit" name="submit" class="btn btnprimary">Perbaharui</button>
- </div>
- </form>
- </div>
- </div>
+<div class="card mb-3">
+  <div class="card-body">
+    <form method="post" action="save-suplier.php">
+      <div class="row">
+        <div class="mb-3 col-md-6">
+          <label for="nama_suplier" class="form-label">Nama suplier</label>
+          <input type="text" class="form-control" name="nama_suplier">
+        </div>
+        <div class="mb-3 col-md-6">
+          <label for="alamat" class="form-label">Alamat</label>
+          <input type="text" class="form-control" name="alamat">
+        </div>
+      </div>
+      <div class="row">
+        <div class="mb-3 col-md-6">
+          <label for="telp" class="form-label">Telp</label>
+          <input type="text" class="form-control" name="telp">
+        </div>
+        <div class="mb-3 col-md-6">
+          <label for="email" class="form-label">Email</label>
+          <input type="text" class="form-control" name="email">
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col text-end">
+          <button class="btn btn-secondary" type="reset">Reset</button>
+          <button class="btn btn-primary" type="submit">Simpan</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
+<div class="card">
+  <div class="card-header">
+    <h3>Data Suplier</h3>
+  </div>
+  <div class="card-body">
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nama Suplier</th>
+            <th>Alamat</th>
+            <th>Telp</th>
+            <th>Email</th>
+            <th><i class="bi bi-gear-fill"></i></th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $conn = mysqli_connect("localhost", "username", "password", "database_name");
+          $query = "SELECT * FROM suplier";
+          $result = mysqli_query($conn, $query);
+          while ($row = mysqli_fetch_assoc($result)) {
+          ?>
+            <tr>
+              <td><?= $row['id'] ?></td>
+              <td><?= $row['nama_suplier'] ?></td>
+              <td><?= $row['alamat'] ?></td>
+              <td><?= $row['telp'] ?></td>
+              <td><?= $row['email'] ?></td>
+              <td>
+                <a href="#editSuplier" class="text-decoration-none" data-bstoggle="modal">
+                  <i class="bi bi-pencil-square text-success"></i>
+                </a>
+                <a href="delete-suplier.php?id=<?= $row['id'] ?>" class="text-decoration-none">
+                  <i class="bi bi-trash text-danger"></i>
+                </a>
+              </td>
+            </tr>
+          <?php
+          }
+          mysqli_close($conn);
+          ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
